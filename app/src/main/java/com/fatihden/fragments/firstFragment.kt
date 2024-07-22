8b51d3829c2e01c8fdb61798fab448336fb0654a
Fatih Dengiz <dengizfth@gmail.com>
2024-07-22 22:06:16 +0300

    binding yapısı Düzenlendi

--- app/src/main/java/com/fatihden/fragments/firstFragment.kt
+++ app/src/main/java/com/fatihden/fragments/firstFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.fatihden.fragments.databinding.FragmentFirstBinding
 
 
 class firstFragment : Fragment() {
+    private var _binding : FragmentFirstBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +22,23 @@ class firstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        /*return inflater.inflate(R.layout.fragment_first, container, false)*/
+        _binding = FragmentFirstBinding.inflate(inflater,container , false)
+        val view = binding.root
+        return view
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        _binding = null
+
     }
 
 }
